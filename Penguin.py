d2aa00babff1cc878b031bef5b9b1b8c6c8a9f61
dediegod <106017815+dediegod@users.noreply.github.com>
2022-05-22 14:13:06 +0200

    Add files via upload

---
+++ Penguin.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import streamlit as st
+import plotly.express as px  # interactive charts
+ 
+url = 'C:/Users/dedie/Desktop/Diseño y programación de herramientas analíticas/ACTIVIDADES/penguins lter.csv'
+df = pd.read_csv(url, sep=",")
+#print(df.head(5))
+
+primaryColor="#6eb1ff"
+backgroundColor="#f1eded"
+secondaryBackgroundColor="#e4e4e4"
+textColor="#000b4c"
+
+#print('Cantidad de Filas y columnas:',df.shape)
+#print('Nombre columnas:',df.columns)
+#df.info()
+
+df["Sex"] = df["Sex"].fillna("Sexo sin identificar")
+df["Comments"] = df["Comments"].fillna("")
+df["Sex"] = df["Sex"].replace(to_replace = ["."], value ="Sexo sin identificar")
+df["Sex"] = df["Sex"].replace(to_replace = ["MALE"], value ="Masculino")
+df["Sex"] = df["Sex"].replace(to_replace = ["FEMALE"], value ="Femenino")
+df["Sex"] = df["Sex"].dropna()
+df = df.dropna()
+df = df.drop(['Sample Number', 'Region'], axis=1)
+
+
+st.set_page_config(
+    page_title="Pingüinos Archipiélago Palmer",
+    page_icon="🐧",
+    layout="wide",
+)
+st.title('Datos de pingüinos del Archipiélago Palmer (Antártida)')
+st.subheader("Autor: Diego de Diego González")
+st.header("Introducción")
+st.markdown('Los datos fueron recopilados y puestos a disposición por la Dra. Kristen Gorman y la Estación Palmer, Antártida LTER , miembro de la Red de Investigación Ecológica a Largo Plazo.')
+#st.markdown("> ")
+st.subheader('Primero seleccione qué campos quiere filtrar en la columna de la izquierda, después seleccione qué desea mostrar:')
+
+
+
+st.sidebar.title('Filtros')
+st.sidebar.write('Puede elegir una o multiples opciones por cada desplegable')
+
+
+container = st.sidebar.container()
+
+all_specie = container.checkbox("Seleccionar todas las especies")
+
+if all_specie:
+    selected_Species = container.multiselect("Selecciona una o más especies de pingüino",
+         pd.unique(df["Species"]), pd.unique(df["Species"]))
+else:
+    selected_Species =  container.multiselect("Selecciona una o más especies de pingüino:",
+        options=(pd.unique(df["Species"])))
+Species = df['Species'].isin(selected_Species)
+df = df[Species]
+
+
+all_Island = container.checkbox("Seleccionar todas las islas")
+if all_Island:
+    selected_Island = container.multiselect("Selecciona una o más islas del pingüino:",
+         pd.unique(df["Island"]), pd.unique(df["Island"]))
+else:
+    selected_Island =  container.multiselect("Selecciona una o más islas del pingüino:",
+        options=(pd.unique(df["Island"])))
+Island = df['Island'].isin(selected_Island)
+df = df[Island]
+
+
+all_Sex = container.checkbox("Seleccionar ambos sexos")
+if all_Sex:
+    selected_Sex = container.multiselect("Seleccionel sexo del pingüino:",
+         pd.unique(df["Sex"]), pd.unique(df["Sex"]))
+else:
+    selected_Sex =  container.multiselect("Seleccionel sexo del pingüino:",
+        options=(pd.unique(df["Sex"])))
+Sex = df['Sex'].isin(selected_Sex)
+df = df[Sex]
+
+
+   
+mostrar_tabla = st.checkbox('Mostrar tabla de datos')
+if mostrar_tabla:
+        st.header("Tabla dataset")
+        st.dataframe (df)
+        
+mostrar_graficos = st.checkbox('Mostrar gráficas')
+if mostrar_graficos:
+    fig_col1, fig_col2 = st.columns(2)
+    with fig_col1:
+        st.markdown("### Body Mass (g)")
+        fig = px.histogram(df, x='Island', y='Flipper Length (mm)', hover_data=['Culmen Length (mm)', 'Culmen Depth (mm)'], color='Sex', barmode='group', height=450)
+        st.write(fig)
+           
+    with fig_col2:
+        st.markdown("### Flipper Length (mm)")
+        fig2 = px.histogram(df, x="Flipper Length (mm)", color='Sex', barmode='group', height=450)
+        st.write(fig2)
+    
+#with fig_col3:
+#st.markdown("### Clutch Completion")
+#fig3 = px.histogram(df, x="Clutch Completion",histfunc='avg')
+#st.write(fig3)
+    
